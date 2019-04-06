4f30241d21dd60248d4433eb8725dd9d340e1ace
tsurumiii <tsurumi0302@gmail.com>
2019-04-06 23:20:16 +0900

    add widget

---
+++ lib/widgets/weather.dart
@@ -0,0 +1,103 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'dart:async';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import './widgets.dart';
+import '../repositories/repositories.dart';
+import '../bloc/bloc.dart';
+
+class Weather extends StatefulWidget {
+  final WeatherRepository weatherRepository;
+  Weather({Key key, @required this.weatherRepository})
+      : assert(weatherRepository != null),
+        super(key: key);
+
+  @override
+  State<Weather> createState() => _WeatherState();
+}
+
+class _WeatherState extends State<Weather> {
+  WeatherBloc _weatherBloc;
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    _weatherBloc = WeatherBloc(weatherRepository: widget.weatherRepository);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Flutter Weather'),
+        actions: <Widget>[
+          IconButton(
+            icon: Icon(Icons.search),
+            onPressed: () async {
+              final city = await Navigator.push(
+                context,
+                CupertinoPageRoute(
+                  builder: (context) => CitySeloction(),
+                ),
+              );
+              if (city != null) {
+                _weatherBloc.dispatch(FetchWeather(city: city));
+              }
+            },
+          ),
+        ],
+      ),
+      body: Center(
+        child: BlocBuilder(
+          bloc: _weatherBloc,
+          builder: (_, WeatherState state) {
+            if (state is WeatherEmpty) {
+              return Center(
+                child: Text('ロケーションを選んで下さい'),
+              );
+            }
+            if (state is WeatherLoading) {
+              return Center(
+                child: CircularProgressIndicator(),
+              );
+            }
+            if (state is WeatherLoaded) {
+              final weather = state.weather;
+
+              return ListView(
+                children: <Widget>[
+                  Padding(
+                    padding: EdgeInsets.only(top: 100),
+                    child: Center(
+                      child: Location(location: weather.location),
+                    ),
+                  ),
+                  Center(
+                    child: LastUpdated(datatime: weather.lastUpdated),
+                  ),
+                  Padding(
+                    padding: EdgeInsets.symmetric(vertical: 50),
+                    child: Center(
+                      child: CombinedWeatherTemperature(
+                        weather: weather,
+                      ),
+                    ),
+                  ),
+                ],
+              );
+            }
+            if (state is WeatherError) {
+              return Text(
+                'エラー！！！！！！！！',
+                style: TextStyle(
+                  color: Colors.red,
+                ),
+              );
+            }
+          },
+        ),
+      ),
+    );
+  }
+}
