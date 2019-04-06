7cf163828cecc426897678e601b9782c41ba64f5
tsurumiii <tsurumi0302@gmail.com>
2019-04-07 01:39:42 +0900

    edit image

--- lib/widgets/weather_conditions.dart
+++ lib/widgets/weather_conditions.dart
@@ -17,32 +17,32 @@ class WeatherConditions extends StatelessWidget {
     switch (condition) {
       case WeatherCondition.clear:
       case WeatherCondition.lightCloud:
-        image =
-            Image.network('https://gyazo.com/9f403d820a39f0d4fd918594afdb8b30');
+        image = Image.network(
+            'https://github.com/felangel/bloc/blob/master/examples/flutter_weather/assets/clear.png?raw=true');
         break;
       case WeatherCondition.hail:
       case WeatherCondition.snow:
       case WeatherCondition.sleet:
-        image =
-            Image.network('https://gyazo.com/e741d19e97fb1ff2d1747329531c8312');
+        image = Image.network(
+            'https://github.com/felangel/bloc/blob/master/examples/flutter_weather/assets/snow.png?raw=true');
         break;
       case WeatherCondition.heavyCloud:
-        image =
-            Image.network('https://gyazo.com/7bda4d1d14dd54488886fa141dd73dc0');
+        image = Image.network(
+            'https://github.com/felangel/bloc/blob/master/examples/flutter_weather/assets/cloudy.png?raw=true');
         break;
       case WeatherCondition.heavyRain:
       case WeatherCondition.lightRain:
       case WeatherCondition.showers:
-        image =
-            Image.network('https://gyazo.com/15c67f9ad8ed88a4a8b3700634370cc1');
+        image = Image.network(
+            'https://github.com/felangel/bloc/blob/master/examples/flutter_weather/assets/rainy.png?raw=true');
         break;
       case WeatherCondition.thunderstorm:
-        image =
-            Image.network('https://gyazo.com/1c7a069cbfdf149a6f55abb52abf54d5');
+        image = Image.network(
+            'https://github.com/felangel/bloc/blob/master/examples/flutter_weather/assets/thunderstorm.png?raw=true');
         break;
       case WeatherCondition.unknown:
-        image =
-            Image.network('https://gyazo.com/9f403d820a39f0d4fd918594afdb8b30');
+        image = Image.network(
+            'https://github.com/felangel/bloc/blob/master/examples/flutter_weather/assets/clear.png?raw=true');
         break;
     }
     return image;
